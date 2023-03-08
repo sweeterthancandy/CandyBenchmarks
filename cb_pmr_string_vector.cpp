4cee039e9d3ffff77141fb3d8bc582b2a1018802
Gerry Candy <g3candy@gmail.com>
2023-03-08 10:00:22 +0000

    wip

--- cb_pmr_string_vector.cpp
+++ cb_pmr_string_vector.cpp
@@ -8,9 +8,31 @@
 #include <algorithm>
 #include <utility>
 #include <iterator>
+#include <array>
 #include <memory_resource>
 
-constexpr const char* prototype_long_text =  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+constexpr const char prototype_long_text[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+constexpr size_t min_strings = 2 << 8;
+constexpr size_t max_strings = 2 << 10;
+
+static void PureMemcopy(benchmark::State& state) {
+        
+        
+        std::array<
+                std::array<char, sizeof(prototype_long_text)>,
+                max_strings
+        > A; 
+        
+        for (auto _ : state) {
+                for (size_t idx = 0; idx != max_strings; ++idx)
+                {
+                        benchmark::DoNotOptimize(std::memcpy(A[idx].data(), prototype_long_text, sizeof(prototype_long_text)));
+                }
+        }
+}
+BENCHMARK(PureMemcopy);
+
 
 static void StdStringVector(benchmark::State& state) {
         const auto num_strings = state.range(0);
@@ -23,7 +45,10 @@ static void StdStringVector(benchmark::State& state) {
                 }
         }
 }
-BENCHMARK(StdStringVector)->RangeMultiplier(2)->Range(8, 2 << 10);
+
+BENCHMARK(StdStringVector)->RangeMultiplier(2)->Range(min_strings, max_strings);
+
+
 
 static void PmrStringPmrVector_NoAllocator(benchmark::State& state) {
         const auto num_strings = state.range(0);
@@ -36,35 +61,83 @@ static void PmrStringPmrVector_NoAllocator(benchmark::State& state) {
                 }
         }
 }
-BENCHMARK(PmrStringPmrVector_NoAllocator)->RangeMultiplier(2)->Range(8, 2 << 10);
+BENCHMARK(PmrStringPmrVector_NoAllocator)->RangeMultiplier(2)->Range(min_strings, max_strings);
 
 
 static void PmrStringPmrVector_MonotonicBuffer(benchmark::State& state) {
         const auto num_strings = state.range(0);
 
-        std::vector<std::byte> buf(num_strings * sizeof(prototype_long_text) + 1);
-        std::pmr::monotonic_buffer_resource pool{ buf.data(), buf.size() };
-
         for (auto _ : state) {
-                std::pmr::vector<std::pmr::string> V{ &pool };
+                
+                std::pmr::monotonic_buffer_resource mbr;
+                std::pmr::vector<std::pmr::string> V{ 0, &mbr };
+                V.reserve(num_strings);
                 for (size_t idx = 0; idx != num_strings; ++idx)
                 {
-                        V.push_back(std::pmr::string(prototype_long_text, &pool));
+                        V.emplace_back(prototype_long_text);
                 }
         }
 }
-BENCHMARK(PmrStringPmrVector_MonotonicBuffer)->RangeMultiplier(2)->Range(8, 2 << 10);
+BENCHMARK(PmrStringPmrVector_MonotonicBuffer)->RangeMultiplier(2)->Range(min_strings, max_strings);
 
-static void Dummy(benchmark::State& state) {
+
+static void PmrStringPmrVector_MonotonicBuffer_PreAlloc(benchmark::State& state) {
         const auto num_strings = state.range(0);
 
-        std::vector<std::byte> buf(num_strings * sizeof(prototype_long_text) + 1);
-        std::pmr::monotonic_buffer_resource pool{ buf.data(), buf.size() };
+
+        constexpr size_t buffer_size = 1024 * 1024 * 16;
+        std::vector<std::byte> buf(buffer_size);
 
         for (auto _ : state) {
+
+                std::pmr::monotonic_buffer_resource pool{ buf.data(), buf.size(), std::pmr::null_memory_resource() };
+                std::pmr::vector<std::pmr::string> V{ 0, &pool };
+                V.reserve(num_strings);
+                for (size_t idx = 0; idx != num_strings; ++idx)
+                {
+                        V.emplace_back(prototype_long_text);
+                }
         }
 }
-BENCHMARK(Dummy)->RangeMultiplier(2)->Range(8, 2 << 10);
+BENCHMARK(PmrStringPmrVector_MonotonicBuffer_PreAlloc)->RangeMultiplier(2)->Range(min_strings, max_strings);
+
+static void PmrStringPmrVector_SynchronizedPoolResource(benchmark::State& state) {
+        const auto num_strings = state.range(0);
+
+        for (auto _ : state) {
+
+                std::pmr::synchronized_pool_resource resource;
+                std::pmr::vector<std::pmr::string> V{ 0, &resource };
+                V.reserve(num_strings);
+                for (size_t idx = 0; idx != num_strings; ++idx)
+                {
+                        V.emplace_back(prototype_long_text);
+                }
+        }
+}
+BENCHMARK(PmrStringPmrVector_SynchronizedPoolResource)->RangeMultiplier(2)->Range(min_strings, max_strings);
+
+static void PmrStringPmrVector_UnsynchronizedPoolResource(benchmark::State& state) {
+        const auto num_strings = state.range(0);
+
+        for (auto _ : state) {
+
+                std::pmr::unsynchronized_pool_resource resource;
+                std::pmr::vector<std::pmr::string> V{ 0, &resource };
+                V.reserve(num_strings);
+                for (size_t idx = 0; idx != num_strings; ++idx)
+                {
+                        V.emplace_back(prototype_long_text);
+                }
+        }
+}
+BENCHMARK(PmrStringPmrVector_UnsynchronizedPoolResource)->RangeMultiplier(2)->Range(min_strings, max_strings);
+
+
+
+
+
+
 
 BENCHMARK_MAIN();
 
